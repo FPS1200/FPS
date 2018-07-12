098c240a9488c5e149bf0dd4788650c362f8b68b
FPS1200 <40477526+FPS1200@users.noreply.github.com>
2018-07-12 19:34:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ function carregardados() {
     xhttp.open("GET", "/dados/produtos.json", true);
     xhttp.send();
   }
-
-
   function preencheHTML(){
+      const produto = produtos[1];
+      const produto = produtos[2];
+
       document.querySelector("#titulo").innerText = produto.titulo;
       document.querySelector("#descricao").innerText = produto.descricao;
-      document.querySelector("#imagem1").innerImagem1 = produto.imagem1;
+      document.querySelector("#preco").innerText = produto.preco$;
+      document.querySelector("#imagem-ativa").src = produto.imagens[0];
+      document.querySelector("#imagem1").src = produto.imagens[0];
+      document.querySelector("#imagem2").src = produto.imagens[1];
+      document.querySelector("#imagem3").src = produto.imagens[2];
+      document.querySelector("#imagem4").src = produto.imagens[3];
+    
 
   }
   carregarDados();
