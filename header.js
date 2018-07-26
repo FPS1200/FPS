9a0cf5ac5f28cfb9a22253ca949f16ec4bbaa1f1
FPS1200 <40477526+FPS1200@users.noreply.github.com>
2018-07-26 19:34:28 -0300

    Add files via upload

--- header.js
+++ header.js
@@ -1,12 +1,27 @@
+$(document).ready(function () {
+    $("#form-search").submit(function (e) {
+        e.preventDefault();
+        const search = $("#input-search").val();
+        const cards = $(".card");
+        cards.each(function () {
+            const elemento = $(this);
+            if (!elemento.text().includes(search)) {
+                elemento.fadeOut();
+            }
+        });
+    })
+})
+
+window.onload
+
 function carregarHeader() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.querySelector("#header").innerHTML = this.responseText;
+    $.ajax({
+        url: "/header.html",
+        method: "GET",
+        success: function (results) {
+            $("#header").html(results);
         }
-    };
-    xhttp.open("GET", "/header.html", true);
-    xhttp.send();
+    });
 }
 
 carregarHeader();
